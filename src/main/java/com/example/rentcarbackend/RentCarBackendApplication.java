d3ad4ccc939eb0799d0708d44f8f1ad85330831a
Sebastian <sebaszymek@gmail.com>
2022-07-26 18:21:26 +0200

    first commit

---
+++ src/main/java/com/example/rentcarbackend/RentCarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentcarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentcarbackend/RentCarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentcarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
